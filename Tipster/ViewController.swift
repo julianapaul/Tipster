b19021456859b3ff4632a048694bb9c4112596a5
Juliana Paul <paul.juliana15@gmail.com>
2020-01-19 21:01:12 -0500

    Initial Commit

---
+++ Tipster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tipster
+//
+//  Created by Juliana Paul on 1/19/20.
+//  Copyright © 2020 Juliana Paul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
